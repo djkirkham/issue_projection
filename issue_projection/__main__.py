9c117ec5c6956dfaf987a389bd4f1b54b56104a3
Daniel Kirkham <daniel.kirkham@metoffice.gov.uk>
2017-08-11 14:48:58 +0100

    Get issue for pull request

--- issue_projection/__main__.py
+++ issue_projection/__main__.py
@@ -47,6 +47,11 @@ def get_projects(user, repo, label=None):
     return result
 
 
+def get_issue(user, repo, number):
+    target = '/repos/{}/{}/issues/{}'.format(user, repo, number)
+    return get_content(target, accept='application/vnd.github.inertia-preview+json')
+
+
 def get_project_columns(project):
     target = '/projects/{}/columns'.format(project['id'])
     return get_content(target, accept='application/vnd.github.inertia-preview+json')
@@ -85,7 +90,8 @@ def filter_columns_issues(columns, issues):
     return result
 
 
-def log_labeled_issue(index, payload):
+def log_labeled_issue(event, payload):
+    index = 'issue' if event == 'issues' else 'pull_request'
     user = payload['sender']['login']
     issue_number = payload[index]['number']
     issue_title = payload[index]['title']
@@ -113,16 +119,19 @@ class PayloadHandler(tornado.web.RequestHandler):
             payload = tornado.escape.json_decode(self.request.body)
             logging.info(payload)
             if event in ('issues', 'pull_request'):
-                index = 'issue' if event == 'issues' else 'pull_request'
                 action = payload['action']
                 logging.info(action)
                 label = 'bug'
                 issue_label = payload['label']['name']
                 if action == 'labeled' and issue_label == label:
-                    log_labeled_issue(index, payload)
+                    log_labeled_issue(event, payload)
                     repo_owner = payload['repository']['owner']['login']
                     repo_name = payload['repository']['name']
-                    issue = payload[index]
+                    if event == 'pull_request':
+                        number = payload['number']
+                        issue = get_issue(repo_owner, repo_name, number)
+                    else:
+                        issue = payload['issue']
                     project = get_projects(repo_owner, repo_name, label=label)
                     columns = get_project_columns(project)
                     column, = [column for column in columns
